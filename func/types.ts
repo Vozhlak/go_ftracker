b39a55b4cc90f223a6de156179f13db09764371f
Ilya Besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2024-05-18 17:12:10 +0600

    feat(f-3): added types for the function

---
+++ func/types.ts
@@ -0,0 +1,6 @@
+type SumFn = (a: number, b: number) => number;
+type SubFn = (a: number, b: number) => number;
+type DivFn = (a: number, b: number) => number;
+type MulFn = (a: number, b: number) => number;
+
+export type {SumFn, SubFn, DivFn, MulFn};
