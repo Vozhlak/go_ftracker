df0ec4f438928d689c914713f29290ac8b0469b2
Ilya Besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2024-05-18 17:09:20 +0600

    refactor(f-2): I refactored the function and the result is

--- func/index.ts
+++ func/index.ts
@@ -1,8 +1,10 @@
-const sum = (a: number, b: number) => {
+import { DivFn, MulFn, SubFn, SumFn } from "./types";
+
+const sum: SumFn = (a, b) => {
     return a + b;
 }
 
-const div = (a: number, b: number) => {
+const div: DivFn = (a: number, b: number) => {
     if (b === 0) {
         throw new Error('на ноль нельзя делить!!!');
     }
@@ -10,19 +12,29 @@ const div = (a: number, b: number) => {
     return a / b;
 }
 
-const sub = (a: number, b: number) => {
+const sub: SubFn = (a: number, b: number) => {
     return a - b;
 }
 
-const mul = (a: number, b: number) => {
+const mul: MulFn = (a: number, b: number) => {
     return a * b;
 }
 
 const result = (actions: 'sum' | 'sub' | 'div' | 'mul') => {
-    if (actions === 'sum') {
-        return (a: number, b: number): number => {
+    switch(actions) {
+        case 'sum': (a: number, b: number) => {
             return sum(a, b);
         }
+        case 'sub': (a: number, b: number) => {
+            return sub(a, b);
+        }
+        case 'mul': (a: number, b: number) => {
+            return mul(a, b);
+        }
+        case 'div': (a: number, b: number) => {
+            return div(a, b);
+        }
+        default: return 'Unknown action!!!';
     }
 }
 
